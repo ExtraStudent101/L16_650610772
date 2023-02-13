e8895a56205769b7da388c56acdfa7ca4c9c05ec
E4rThCPEcmu65 <110962678+ExtraStudent101@users.noreply.github.com>
2023-02-13 21:32:02 +0700

    lab16_4_finished

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,24 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d) 
+{
+    int w = 0,x = 0,y = 0,z = 0;
+	int dang[4] = {a,b,c,d};
+	
+	w = rand()%4;
+    a = dang[w];
+
+	while(x == w){
+	 x = rand()%4;
+	}b = dang[x];
+
+	while(y == w or y == x){
+	 y = rand()%4;
+	}c = dang[y];
+
+    while(z == x or z == w or z == y){
+	 z = rand()%4;
+	}d = dang[z];
+}
